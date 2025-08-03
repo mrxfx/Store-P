50487818662fd19ec479925a8a4e5e13a0d96a74
mrxfx <mrxfzyz143@gmail.com>
2025-08-03 09:28:35 +0530

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getStorage, ref as storageRef, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPF1VE82Y3VkZe6IibjqKxBC-XHjM_Wco",
+  authDomain: "chat-2024-ff149.firebaseapp.com",                    // default auth domain
+  databaseURL: "https://chat-2024-ff149-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-2024-ff149",
+  storageBucket: "chat-2024-ff149.appspot.com",
+  messagingSenderId: "146349109253",
+  appId: "1:146349109253:android:e593afbf0584762519ac6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const storage = getStorage(app);
+
+// Export helpers for the rest of your modules
+export { db, storage, ref, get, child, storageRef, getDownloadURL };
